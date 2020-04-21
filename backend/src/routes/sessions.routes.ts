e10e204a3e565ac3e549547d33ade3ed49612a81
DaniloParkour <danilomlcarvalho@gmail.com>
2020-04-20 22:07:25 -0300

    Autenticação Funcionando

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({ email, password });
+
+    // Nunca responder com o retorno do service diretamente pois o retorno pode mudar depois e conter informações que não deveriam ir na resposta
+    // Usar sempre nomes de variáveis bem descritivas
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
