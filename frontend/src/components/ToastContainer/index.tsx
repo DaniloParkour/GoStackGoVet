4a8c88a93dc543215fd92f9f21ee8601e852917b
DaniloParkour <danilomlcarvalho@gmail.com>
2020-05-06 11:20:59 -0300

    Toasts animados ao entrar e sair. Animações feitas com react-spring

--- frontend/src/components/ToastContainer/index.tsx
+++ frontend/src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 import { Container } from './styles';
 import { ToastMessage } from '../../hooks/toast';
 import Toast from './Toast';
@@ -8,10 +9,23 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messageWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-120%', opacity: 0, transform: 'rotateX(0deg)' }, // 120% do tamanho dele a partir da posição que ele está
+      enter: { right: '0%', opacity: 1, transform: 'rotateX(360deg)' },
+      leave: { right: '-120%', opacity: 0, transform: 'rotateX(0deg)' },
+      // leave: { right: '-120%', opacity, ...CSSoptions },
+    },
+  );
+
   return (
     <Container>
-      {messages.map((message) => (
-        <Toast key={message.id} message={message} />
+      {messageWithTransitions.map((
+        { item, key, props }, // Props são as propriedades de estilização
+      ) => (
+        <Toast style={props} key={key} message={item} />
       ))}
     </Container>
   );
