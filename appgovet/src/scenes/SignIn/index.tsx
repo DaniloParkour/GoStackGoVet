55247a30815031dcac3d4793bd585d357ea65d61
DaniloParkour <danilomlcarvalho@gmail.com>
2020-05-16 11:35:21 -0300

    Agora está validando os campor do form e deixando em vermelho quando errado

--- appgovet/src/scenes/SignIn/index.tsx
+++ appgovet/src/scenes/SignIn/index.tsx
@@ -1,14 +1,22 @@
 import React, {useCallback, useRef} from 'react';
-import {Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput} from 'react-native';
+import {Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput, Alert} from 'react-native';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Icon from 'react-native-vector-icons/Feather';
 import {useNavigation} from '@react-navigation/native';
 import {Form} from '@unform/mobile';
 import {FormHandles} from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.png';
 import {Container, Title, ForgotPassword, ForgotPasswordText,  CreateAccountButton, CreateAccountButtonText} from './styles';
+
+interface SignFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn:React.FC = () => {
 
@@ -18,9 +26,55 @@ const SignIn:React.FC = () => {
 
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data);
-  }, []);
+  const handleSignIn = useCallback(
+    async (data: SignFormData) => {
+      // schema de validação é usado para validar objetos inteiros e não campo por campo
+      try {
+        // Zerar os erros mostrados no fomrulário
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Email inválido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        // await schema.validate(data);
+        await schema.validate(data, {
+          // Com o abortEarly falso a gente pode ver toda a varificação e receber de uma vez todos os erros
+          abortEarly: false, // Foi colocado para não parar no primeiro erro. Por padrão o Yup para no primeiro erro
+        });
+
+        /*await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        */
+
+        //history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          /* formRef.current?.setErrors({
+            name: 'Nome obrigatório',
+          }); */
+          return;
+        }
+
+        Alert.alert('Erro na validação', 'Ocorreu um erro no login, verifique se o email e senha estão corretos');
+
+        /*addToast({
+          type: 'info',
+          title: 'Erro no login',
+          description:
+            'Ocorreu um erro no login, verifique se o email e senha estão corretos',
+        });*/
+      }
+    },
+    [],
+  );
 
   return(
     <KeyboardAvoidingView
--- appgovet/src/scenes/SignUp/index.tsx
+++ appgovet/src/scenes/SignUp/index.tsx
@@ -1,14 +1,22 @@
-import React, {useRef} from 'react';
-import {Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput} from 'react-native';
+import React, {useRef, useCallback} from 'react';
+import {Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput, Alert} from 'react-native';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Icon from 'react-native-vector-icons/Feather';
 import {useNavigation} from '@react-navigation/native';
 import {Form} from '@unform/mobile';
 import {FormHandles} from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.png';
 import {Container, Title, BackToSignIn, BackToSignInText} from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp:React.FC = () => {
 
@@ -16,6 +24,63 @@ const SignUp:React.FC = () => {
   const mailInputRef = useRef<TextInput>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      // schema de validação é usado para validar objetos inteiros e não campo por campo
+      try {
+        // Zerar os erros mostrados no fomrulário
+        formRef.current?.setErrors({});
+
+        //Os nomes aqui devem ser o mesmo nome do campo (passado pela props name)
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório.'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Email inválido'),
+          password: Yup.string().min(
+            6,
+            'A senha deve conter no mínimo 6 dígitos',
+          ),
+        });
+
+        // await schema.validate(data);
+        await schema.validate(data, {
+          // Com o abortEarly falso a gente pode ver toda a varificação e receber de uma vez todos os erros
+          abortEarly: false, // Foi colocado para não parar no primeiro erro. Por padrão o Yup para no primeiro erro
+        });
+
+        //await api.post('/users', data);
+
+        /*addToast({
+          type: 'success',
+          title: 'Cadastro realizado',
+          description: 'Você já pode fazer seu logon no GoVet',
+        });
+
+        history.push('/');*/
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          /* formRef.current?.setErrors({
+          name: 'Nome obrigatório',
+        }); */
+          return;
+        }
+
+        Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer cadastro, tente novamente');
+        /*
+        addToast({
+          type: 'info',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer cadastro, tente novamente',
+        });
+        */
+      }
+    },
+    [],
+  );
 
   return(
     <KeyboardAvoidingView
@@ -33,10 +98,10 @@ const SignUp:React.FC = () => {
             <Title>Crie Sua Conta</Title>
           </View>
 
-          <Form ref={formRef} onSubmit={(data) => {console.log(data)}}>
+          <Form ref={formRef} onSubmit={handleSignUp}>
             <Input
               autoCapitalize="words"
-              name='nome'
+              name='name'
               icon='user'
               placeholder='Nome'
               returnKeyType='next'
